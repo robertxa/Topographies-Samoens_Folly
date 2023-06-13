4b6c7b7fbbf5783169ff6db47f2cb8854f6fc715
robertxa <xavier.robert@univ-grenoble-alpes.fr>
2023-06-13 18:08:45 +0200

    Update GPKG

--- Build-Samoens-SIG.sh
+++ Build-Samoens-SIG.sh
@@ -26,13 +26,13 @@ echo ''
 # 1- Générer les shapefiles 3D des différentes topographies de tout le système
 echo '    1- Construction des shapefiles 3D et des listes de cavités...'
 echo '          Attention, cette étape est longue !!!'
-#therion SamoensGIS.thconfig
+therion SamoensGIS.thconfig
 echo ''
 # 2- Construire ou mettre à jour les shapefiles des entrées de cavités majeures
 echo '    2- Travail sur les shapefiles des entrées de cavités majeures....'
 #cd Outputs/Lists/Therion-ShpEntrees
 cd Samoens-GIS/Scripts
-#python pythEntrance.py
+python pythEntrance.py
 ##pythEntrance.py:571: FionaDeprecationWarning: This function will be removed in version 2.0. Please use CRS.from_epsg() instead.
 ## Revoir les entrées du Criou !!!
 #cd -
@@ -40,7 +40,7 @@ echo ''
 # 3- Mise à jour des shapefiles stations3D pour rajouter les informations d'altitudes
 echo '    3- Mise à jour des shapefiles stations3D (rajout des altitudes à la table attributaire)'
 ##cd Outputs/3D/Folly-tot-3D
-#python pyStationsAltitude.py
+python pyStationsAltitude.py
 #cd -
 echo ''
 # 4- Mise à jour des shapefiles stations3D pour rajouter les informations d'altitudes
